42cd6cf42c746223462187ffdca75f5e55ee9634
lux <lookateach1@gmail.com>
2019-06-03 16:41:23 +0100

    Initial Commit

---
+++ FundingCircle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FundingCircle
+//
+//  Created by Tomislav Luketic on 6/3/19.
+//  Copyright © 2019 lux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
